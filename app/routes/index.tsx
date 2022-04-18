00e7d01695061a46396fa24f0f76d9d7c94f854f
Рахматуллин Альберт Рахимуллович <rakhmatullin.ar@esphere.ru>
2022-04-18 23:08:06 +0300

    форма

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,25 +2,27 @@ import type { LinksFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
 import stylesUrl from "~/styles/index.css";
 
-export const links: LinksFunction = () =>{
-    return [{rel: "stylesheet", href: stylesUrl}];
-}
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
 
 export default function IndexRoute() {
-    return (
-        <div className="container">
-            <div className="content">
-                <h1>
-                Remix <span>Jokes!</span>
-                </h1>
-                <nav>
-                <ul>
-                    <li>
-                        <Link to="jokes" prefetch="render">Read Jokes</Link>
-                    </li>
-                </ul>
-                </nav>
-            </div>
-        </div>
-    )
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes" prefetch="render">
+                Read Jokes
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,23 +1,37 @@
-import type { LinksFunction } from "@remix-run/node";
-import { Outlet, Link } from "@remix-run/react";
+import type { LinksFunction, LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
 
-import stylesUrl from '~/styles/jokes.css';
+import { Outlet, Link, useLoaderData } from "@remix-run/react";
+
+import stylesUrl from "~/styles/jokes.css";
+import { db } from "~/utils/db.server";
+
+type LoaderData = { jokes: Array<{ id: string; name: string }> };
 
 export const links: LinksFunction = () => {
-    return [{rel: 'stylesheet', href: stylesUrl}]
-}
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
+export const loader: LoaderFunction = async () => {
+  const data: LoaderData = {
+    jokes: await db.joke.findMany({
+      take: 5,
+      select: { id: true, name: true },
+      orderBy: { createdAt: "desc" },
+    }),
+  };
+
+  return json(data);
+};
 
 export default function JokesRoute() {
-    return (
-        <div className="jokes-layout">
+  const data = useLoaderData<LoaderData>();
+  return (
+    <div className="jokes-layout">
       <header className="jokes-header">
         <div className="container">
           <h1 className="home-link">
-            <Link
-              to="/"
-              title="Remix Jokes"
-              aria-label="Remix Jokes"
-            >
+            <Link to="/" title="Remix Jokes" aria-label="Remix Jokes">
               <span className="logo">🤪</span>
               <span className="logo-medium">J🤪KES</span>
             </Link>
@@ -30,9 +44,11 @@ export default function JokesRoute() {
             <Link to=".">Get a random joke</Link>
             <p>Here are a few more jokes to check out:</p>
             <ul>
-              <li>
-                <Link to="some-joke-id">Hippo</Link>
-              </li>
+              {data?.jokes?.map((joke) => (
+                <li key={joke.id}>
+                  <Link to={joke.id}>{joke.name}</Link>
+                </li>
+              ))}
             </ul>
             <Link to="new" className="button">
               Add your own
@@ -44,5 +60,5 @@ export default function JokesRoute() {
         </div>
       </main>
     </div>
-    )
+  );
 }
